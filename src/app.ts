376ed435390be3af24c957b457a2b850fc54aeef
joaovs2004 <joaovitorschmittschmitt@gmail.com>
2023-01-15 19:36:27 -0300

    Add municipalities router to app and move db sync

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import express from "express";
 import productsRouter from "./Routes/Products.routes";
+import municipalitiesRouter from "./Routes/Municipalities.routes";
+import db from "./Configs/db";
 
 const app = express();
 
+// I need to synchronize the database, because if it doesn't exist, the queries will give an error
+db.sync().then(() => {
+    console.log("Database synchronized");
+});
+
 app.use(express.json());
 app.use("/Products", productsRouter);
+app.use("/Municipalities", municipalitiesRouter);
 
 app.listen(8080, () => console.log("server running on port 8080"));
