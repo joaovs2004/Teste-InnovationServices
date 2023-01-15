ee04445d5ec6bb754ace48e93ab64b6b1c4d3a46
joaovs2004 <joaovitorschmittschmitt@gmail.com>
2023-01-15 00:21:50 -0300

    Change name of router of products

--- src/Routes/Products.routes.ts
+++ src/Routes/Products.routes.ts
@@ -2,13 +2,13 @@ import { Router } from "express";
 import { listAllProducts, listById, paginatedList, addProduct, deleteProduct, updateProduct } from "../Controllers/Products.controller";
 import { paginatedListValidator, addProductValidator, updateProductValidator } from "../Configs/validators";
 
-const router = Router();
+const productsRouter = Router();
 
-router.get("/listAll", listAllProducts);
-router.get("/listById/:productId", listById);
-router.get("/paginatedList", paginatedListValidator, paginatedList);
-router.post("/add", addProductValidator, addProduct);
-router.delete("/delete/:productId", deleteProduct);
-router.patch("/update/:productId", updateProductValidator, updateProduct);
+productsRouter.get("/listAll", listAllProducts);
+productsRouter.get("/listById/:productId", listById);
+productsRouter.get("/paginatedList", paginatedListValidator, paginatedList);
+productsRouter.post("/add", addProductValidator, addProduct);
+productsRouter.delete("/delete/:productId", deleteProduct);
+productsRouter.patch("/update/:productId", updateProductValidator, updateProduct);
 
-export default router;
+export default productsRouter;
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express from "express";
-import router from "./Routes/Products.routes";
+import productsRouter from "./Routes/Products.routes";
 
 const app = express();
 
 app.use(express.json());
-app.use("/Products", router);
+app.use("/Products", productsRouter);
 
 app.listen(8080, () => console.log("server running on port 8080"));
